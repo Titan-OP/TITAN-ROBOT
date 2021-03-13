0b6a57e2c0a80e39cdfc2b780783d1e5ea0154df
Sasaki Uchiha <62333331+FtSasaki@users.noreply.github.com>
2021-03-13 17:37:12 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -75,13 +75,13 @@ def get_readable_time(seconds: int) -> str:
 PM_START_TEXT = """
 Hi {}, my name is {}! 
 I am an Anime themed group management bot.
-Build by weebs for weebs, I specialize in managing anime and similar themed groups.\
-• You can find my list of available commands with /help.
+Build by weebs for weebs, I specialize in managing anime and similar themed groups.
+You can find my list of available commands with /help.
 """
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm Dr Stone & I help admins to manage their groups! Have a look at the following for an idea of some of \
+I'm Dr Stone & I help admins to manage their groups! Have a look at the following for an idea of some of
 the things I can help you with.
 
 *Main* commands available:
