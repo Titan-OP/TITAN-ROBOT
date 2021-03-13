28748cae3e0866029b840c59ab0ffa7482898146
Sasaki Uchiha <62333331+FtSasuke@users.noreply.github.com>
2021-03-13 16:35:29 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -626,7 +626,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "I am *now* online!")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Get Excited coz I am Online!")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
