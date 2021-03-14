ba92c5ee3179564dec9672b10f7e96c93a1dcc13
Sasaki Uchiha <62333331+FtSasaki@users.noreply.github.com>
2021-03-14 08:17:26 +0530

    kek

--- SaitamaRobot/modules/tools.py
+++ SaitamaRobot/modules/tools.py
@@ -1,22 +1,29 @@
-
 __help__ = f"""
 🎲 *Commands* *:*` 
-
+*Plet*:
 • `/plet` *:* text get funny emojify
+*Image Search*:
 • `/reverse`*:* Does a *reverse image search* of the media which it was replied to.
+*COVID*:
 • `/covid` *:* stats of covid
+*KickFools*:
 • `/kickthefools` *:* kick inactive members
+*Quotly*:
 • `/q` *:* sends a quote of the message replied to
+*Music*:
 • `/song` `<songname artist(optional)>`*:* uploads the song in it's best quality available
 • `/video` `<songname artist(optional)>`*:* uploads the video song in it's best quality available
+*TTS-STT*:
 • `/tts` `<text>`*:* convert text to speech
 • `/stt` `<text>`*:* convert speech to text
+*Zipper*:
 • `/zip` *:* reply to a telegram file to compress it in .zip format
 • `/unzip` *:* reply to a telegram file to decompress it from the .zip format
+*Zombies*:
 • `/zombies` *:* scan deleted accounts
 • `/zombies clean` *:* cleans deleted accounts
+*Time*:
 • `/time <query>`*:* Gives information about a timezone.
-
 *Available queries:* Country Code/Country Name/Timezone Name
 • 🕐 [Timezones list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
  
