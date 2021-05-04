2bef92c67314d437fc39ee544dab58dcc051fd4c
kanekiken44 <81549240+kanekiken44@users.noreply.github.com>
2021-05-04 09:16:38 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -8,7 +8,6 @@ import telegram.ext as tg
 from telethon import TelegramClient
 from pyrogram import Client, errors
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-from Python_ARQ import ARQ
 
 StartTime = time.time()
 
